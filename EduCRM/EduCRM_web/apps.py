165ff3972ae9f45fafddc65036c3774b530c1694
zhang6465385 <48509105+zhang6465385@users.noreply.github.com>
2019-07-12 08:56:33 +0800

    111

--- EduCRM/EduCRM_web/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EducrmWebConfig(AppConfig):
-    name = 'EduCRM_web'
--- EduCRM/EduCRM_web/uploadfile.py
+++
@@ -1,9 +0,0 @@
-import os
-from EduCRM import settings
-
-
-def uploadfile(img):
-    f = open(os.path.join(settings.UPLOAD_ROOT, '', img.name), 'wb')
-    for chunk in img.chunks():
-        f.write(chunk)
-    f.close()
